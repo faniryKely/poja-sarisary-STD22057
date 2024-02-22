4d09ebb4b510d9e7e0633cc849042a33c9ac754f
Faniry <faniryrandria08@gmail.com>
2024-02-22 12:37:03 +0300

    feat: adding jacocovalue

--- src/main/java/hei/school/sarisary/endpoint/rest/controller/health/SaryController.java
+++ src/main/java/hei/school/sarisary/endpoint/rest/controller/health/SaryController.java
@@ -1,4 +1,5 @@
 package hei.school.sarisary.endpoint.rest.controller.health;
+
 import hei.school.sarisary.service.event.SaryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
@@ -7,15 +8,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @AllArgsConstructor
 public class SaryController {
-        private final SaryService saryService;
+  private final SaryService saryService;
 
-        @PutMapping(value = "/black-and-white/{id}", produces = MediaType.IMAGE_PNG_VALUE)
-        public byte[] toBlackAndWhite(@PathVariable String key, @RequestBody byte[] image) {
-            return saryService.convertToBlackAndWhite(image, key);
-        }
-
+  @PutMapping(value = "/black-and-white/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+  public byte[] toBlackAndWhite(@PathVariable String key, @RequestBody byte[] image) {
+    return saryService.convertToBlackAndWhite(image, key);
+  }
 }
